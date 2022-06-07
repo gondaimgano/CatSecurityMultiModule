0b1a86146f8f0703891c1c1bce7d52ec774ececc
gondaimgano <gondaimgano@gmail.com>
2022-06-07 13:45:17 +0200

    SecurityServiceTest refinement

--- security/src/test/java/com/udacity/catpoint/security/SecurityServiceTest.java
+++ security/src/test/java/com/udacity/catpoint/security/SecurityServiceTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
@@ -75,7 +74,7 @@ public class SecurityServiceTest {
 
 
     @ParameterizedTest //covers 1
-    @EnumSource(value = ArmingStatus.class, names = {"ARMED_HOME", "ARMED_AWAY"})
+    @MethodSource("whenSystemArmedReset_setSensorsToInactive")
     void whenAlarmStatus_AlarmIsArmedAndSensorIsActivated_setAlarmStatusPending(ArmingStatus armingStatus) {
         when(securityService.getArmingStatus()).thenReturn(armingStatus);
         when(securityService.getAlarmStatus()).thenReturn(AlarmStatus.NO_ALARM);
